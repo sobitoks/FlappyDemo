6b9371893b7fcf1221aae8f896fb2855da6d6f73
galfonso <gerard.alfonso@sunqu.net>
2018-07-13 13:37:00 +0200

    Flappy demo skeleton

---
+++ desktop/src/com/sobitoks/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sobitoks.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sobitoks.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
